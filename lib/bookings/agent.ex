8a1172b12394210105b03326247fd15630ba630a
Thaís Mendes Ramalho <thaismr@users.noreply.github.com>
2022-06-03 20:24:43 -0300

    Extra reports generator

--- lib/bookings/agent.ex
+++ lib/bookings/agent.ex
@@ -17,10 +17,31 @@ defmodule Flightex.Bookings.Agent do
 
   def list_all, do: Agent.get(__MODULE__, & &1)
 
+  def list_by_date(from_date, to_date),
+    do: Agent.get(__MODULE__, &list_bookings_by_date(&1, from_date, to_date))
+
   defp get_booking(state, uuid) do
     case Map.get(state, uuid) do
       nil -> {:error, "Booking not found"}
       booking -> {:ok, booking}
+    end
+  end
+
+  defp list_bookings_by_date(state, from_date, to_date) do
+    case Enum.filter(state, &booking_between_dates?(&1, from_date, to_date)) do
+      nil -> {:error, "No booking found for given dates"}
+      booking_list -> {:ok, booking_list}
+    end
+  end
+
+  defp booking_between_dates?(%Booking{complete_date: complete_date}, from_date, to_date) do
+    with x <- NaiveDateTime.compare(complete_date, from_date),
+         y <- NaiveDateTime.compare(complete_date, to_date),
+         true <- x in [:gt, :eq],
+         true <- y in [:lt, :eq] do
+      true
+    else
+      false
     end
   end
 
--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -8,10 +8,30 @@ defmodule Flightex.Bookings.Report do
     File.write(filename, booking_list)
   end
 
+  def generate_report(from_date, to_date) do
+    case build_booking_list(from_date, to_date) do
+      nil ->
+        {:error, "Unable to generate a report for given dates"}
+
+      booking_list ->
+        case File.write("report-by-date.csv", booking_list) do
+          {:ok, _report} -> {:ok, "Report generated successfully"}
+          error -> error
+        end
+    end
+  end
+
   defp build_booking_list() do
     BookingAgent.list_all()
     |> Map.values()
     |> Enum.map(&booking_string/1)
+  end
+
+  defp build_booking_list(%NaiveDateTime{} = from_date, %NaiveDateTime{} = to_date) do
+    case BookingAgent.list_by_date(from_date, to_date) do
+      {:ok, booking_list} -> booking_list |> Map.values() |> Enum.map(&booking_string/1)
+      {:error, _reason} -> nil
+    end
   end
 
   defp booking_string(%Booking{
